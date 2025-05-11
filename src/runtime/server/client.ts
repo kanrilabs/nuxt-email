1956a7c80cd3c4547d1312190b6ee173ede046af
Gerben Mulder <github.undergo381@passmail.net>
2025-05-11 12:52:50 +0200

    feat: add options and callbacks to client

--- src/runtime/server/client.ts
+++ src/runtime/server/client.ts
@@ -3,11 +3,19 @@ import type { H3Event } from 'h3'
 import type { MailConfig } from '../types/email'
 import { MailBuilder } from './builder'
 
+interface EmailClientOptions {
+  region?: string
+  endpoint?: string
+  onSend?: (response: Response) => void
+  onSendError?: (error: Error) => void
+}
+
 class EmailClient extends MailBuilder {
   #client: AwsClient
   #endpoint: string
+  #opts: EmailClientOptions
 
-  constructor(event: H3Event, config: MailConfig) {
+  constructor(event: H3Event, config: MailConfig, opts?: EmailClientOptions) {
     super(config)
 
     const { accessKeyId, secretAccessKey, region } = useRuntimeConfig(event).mail
@@ -16,7 +24,8 @@ class EmailClient extends MailBuilder {
       secretAccessKey,
       service: 'ses',
     })
-    this.#endpoint = `https://email.${region}.amazonaws.com/v2/email/outbound-emails`
+    this.#endpoint = opts?.endpoint ?? `https://email.${opts?.region ?? region}.amazonaws.com/v2/email/outbound-emails`
+    this.#opts = opts ?? {}
   }
 
   async send() {
@@ -25,9 +34,15 @@ class EmailClient extends MailBuilder {
       const res = await this.#client.fetch(this.#endpoint, {
         body: JSON.stringify({ Content: { Raw: { Data: content } } }),
       })
+      this.#opts.onSend?.(res.clone())
       return res.json()
     }
     catch (error) {
+      if (error instanceof Error)
+        this.#opts.onSendError?.(error)
+      else
+        this.#opts.onSendError?.(new Error(String(error)))
+
       console.error('Error sending email:', error)
     }
   }
